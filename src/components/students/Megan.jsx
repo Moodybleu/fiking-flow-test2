a170358a969dc4c1a9dc630388d72790d9461902
Moodybleu <meganbenn27@icloud.com>
2022-10-05 16:42:04 -0700

    Gotta love them jokes

--- src/components/students/Megan.jsx
+++ src/components/students/Megan.jsx
@@ -3,8 +3,8 @@ export default function Megan(){
         <>
         <h1>Megan here!</h1>
         <p>
-        At a party, a young wife admonished her husband, “That is the fourth time you’ve gone back for ice cream and cake. Doesn’t it embarrass you?”
-        “Why should it?” answered her spouse. “I keep telling them it’s for you.”
+        At a party, a young wife admonished her husband, “That is the fourth time you have gone back for ice cream and cake. Does it not embarrass you?”
+        “Why should it?” answered her spouse. “I keep telling them that it is for you.”
         </p>
        </>
     )
